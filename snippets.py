3c220a91e3ec4e646f7fdb22d72fa95cf343461a
Tyler Hedegard <tchedegard@gmail.com>
2016-05-18 02:33:22 +0000

    added main function with argparse functions

--- snippets.py
+++ snippets.py
@@ -3,6 +3,7 @@
 # Snippets Assignment
 
 import logging
+import argparse
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
@@ -10,17 +11,15 @@ logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 def put(name, snippet):
     """
     Store a snippet with an associated name.
-
     Returns the name and the snippet
     """
     logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
     return name, snippet
     
 def get(name):
-    """Retrieve the snippet with a given name.
-
+    """
+    Retrieve the snippet with a given name.
     If there is no such snippet, return '404: Snippet Not Found'.
-
     Returns the snippet.
     """
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
@@ -42,3 +41,12 @@ def update(name,snippet):
     """
     logging.error("FIXME: Unimplemented - update({!r},{!r}".format(name,snippet))
     return name, snippet
+    
+def main():
+    """Main function"""
+    logging.info("Constructing parser")
+    parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+    arguments = parser.parse_args()
+
+if __name__ == "__main__":
+    main()
