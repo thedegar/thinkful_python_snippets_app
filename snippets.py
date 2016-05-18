1dc6aa59e4b2a9a61e5fda14ecc93ee9ae42366a
Tyler Hedegard <tchedegard@gmail.com>
2016-05-18 18:57:01 +0000

    changed cursor objects to context managers for clean up

--- snippets.py
+++ snippets.py
@@ -20,15 +20,14 @@ def put(name, snippet):
     Returns the name and the snippet
     """
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    cursor = connection.cursor()
-    try:
-        command = "insert into snippets values (%s, %s)"
-        cursor.execute(command, (name, snippet))
-    except psycopg2.IntegrityError as e:
-        connection.rollback()
-        command = "update snippets set message=%s where keyword=%s"
-        cursor.execute(command, (snippet, name))
-    connection.commit()
+    with connection, connection.cursor() as cursor:
+        try:
+            command = "insert into snippets values (%s, %s)"
+            cursor.execute(command, (name, snippet))
+        except psycopg2.IntegrityError as e:
+            connection.rollback()
+            command = "update snippets set message=%s where keyword=%s"
+            cursor.execute(command, (snippet, name))
     logging.debug("Snippet stored successfully.")
     return name, snippet
     
@@ -39,10 +38,9 @@ def get(name):
     Returns the snippet.
     """
     logging.info("Retrieving snippet {!r}".format(name))
-    cursor = connection.cursor()
-    command = "select message from snippets where keyword = %s"
-    cursor.execute(command,(name,))
-    result = cursor.fetchone()
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select message from snippets where keyword=%s", (name,))
+        result = cursor.fetchone()
     if not result:
         # No snippet was found with that name.
         logging.info("Snippet not found")
