f4b8d8a9e586d13d96540ac533250fc1f2695d4b
Tyler Hedegard <tchedegard@gmail.com>
2016-05-18 19:26:04 +0000

    added catalog function

--- snippets.py
+++ snippets.py
@@ -48,6 +48,21 @@ def get(name):
     logging.debug("Snippet retrieved successfully.")
     return result[0]
     
+def catalog():
+    """
+    Returns a list of keywords in the DB
+    """
+    logging.info("Retrieving keywords from DB")
+    with connection, connection.cursor() as cursor:
+        cursor.execute("select keyword from snippets order by keyword")
+        result = cursor.fetchall()
+    if not result:
+        # No keywords found in the
+        logging.info("No keywords found")
+        return "404: No Keywords Found"
+    logging.debug("Keywords retrieved successfully")
+    return result
+
 def remove(name):
     """
     Remove a snippet with a given name
@@ -82,6 +97,10 @@ def main():
     logging.debug("Constructing get subparser")
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
+    
+    # Subparser for the catalog command
+    logging.debug("Constructing catalog subparser")
+    catalog_parser = subparsers.add_parser("catalog", help="Retrieve all keywords")
 
     arguments = parser.parse_args()
     
@@ -95,6 +114,11 @@ def main():
     elif command == "get":
         snippet = get(**arguments)
         print("Retrieved snippet: {!r}".format(snippet))
+    elif command == "catalog":
+        result = catalog(**arguments)
+        print("Keywords: ")
+        for i in result:
+            print(i[0])
 
 if __name__ == "__main__":
     main()
